544a4417052a3e3a2ffcdb2d4faa0a2684ddded7
Adrian <adry.dabp@gmail.com>
2019-05-26 12:21:41 -0400

    pasos para el login

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View, Image, Dimensions, TextInput} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Logo from './assets/login/logoAgetic.png';
+
+const {width: WIDTH} = Dimensions.get('window');
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.header}>
-          <Image source={require('./assets/login/logoAgetic.png')} style={styles.logoAgetic}></Image>
-        </View>
-        <View style={styles.body}>
-          <TextInput
-           placeholder="Hola"
-          ></TextInput>
+          <Image source={Logo} style={styles.LogoAgetic}></Image>
+          <View style={styles.login}>
+          </View>
         </View>
         <View style={styles.footer}>
-          <Image source={require('./assets/login/backgroundFooter.jpg')} style={[styles.backgroundImage]}></Image>
+          <Icon name="android" size={50} color={'black'}/>
+          <TextInput
+            style={styles.inputText}
+            placeholderTextColor={'rgba(0,0,0,.2)'}
+            placeholder={'Adrian Barra'}
+          />
         </View>
       </View>
     );
@@ -21,51 +27,46 @@ export default class App extends Component {
 }
 
 const styles = StyleSheet.create({
-  header: {
-    flex: 2,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  logoAgetic: {
-    width: 100,
-    height: 100,
-    borderWidth: 2,
-    resizeMode: 'contain',
-    borderStyle: 'solid',
-    borderColor: '#000'
-  },
-  body: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  footer: {
-    flex: 2,
-    alignItems: 'flex-end'
-  },
-  reverse: {
-    transform: [{
-      scaleY: -1
-    }]
-  },
-  backgroundImage: {
-    flex: 1,
-    resizeMode: 'stretch',
-    overflow: 'visible',
-    width: Dimensions.get('window').width
-  },
   container: {
     flex: 1,
+    backgroundColor: 'transparent'
+  },
+  header: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'visible',
+    zIndex: 1,
+    backgroundColor: '#880E4F'
+  },
+  LogoAgetic: {
+    width: 70,
+    height: 70,
+    resizeMode: 'contain'
+  },
+  footer: {
+    flex: 3,
     backgroundColor: '#FFF'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  inputText: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    height: 40,
+    borderRadius: 4,
+    margin: 50,
+    width: 'auto'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
-  },
+  login: {
+    flex: 1,
+    position: 'absolute',
+    borderRadius: 4,
+    borderBottomLeftRadius: 0 ,
+    borderBottomRightRadius: 0,
+    borderTopLeftRadius: 4,
+    borderTopRightRadius: 4,
+    backgroundColor: '#FFF',
+    width: WIDTH - 50,
+    paddingVertical: 15,
+    bottom: 0
+  }
 });
